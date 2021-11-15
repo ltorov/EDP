e84b8bc51ff9e7dd047ef81784eb07d8eeda89c4
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-11-15 17:18:33 -0500

    Add files via upload

--- Punto 3/diferenciasFinitasOnda.m
+++ Punto 3/diferenciasFinitasOnda.m
@@ -1,45 +1,47 @@
 
 %Diferencias finitas para onda
-x1 = 0;
-xr = 1;
-yb = 0;
-yt = 1;
+ax = 0;
+bx = 1;
+at = 0;
+bt = 1;
 M = 20;
 N = 20;
-xr,yb,yt,M ,N
 f=@(x,t) 16*pi^2*sin(pi*x)*cos(4*pi*t); % define input function data
 
 g1=@(x) sin(pi*x); % define boundary values
 
-g2=@(x) 0;
+g2= 0;
 
-g3=@(t) 0;
+g3= 0;
 
-g4=@(t) 0;
+g4= 0;
 
 m=M+1;n=N+1; mn=m*n;
 
-h= (xr-x1)/M;h2=h^2;k=(yt-yb)/4;k2=k^2 ;
 
-x=x1+(0:M)*h; % set mesh values
+h= (bx-ax)/M;
+k=(bt-at)/4;
 
-y=yb+(0:N)*k;
+X=(ax:h:bx);
 
-A=zeros(mn,mn);b=zeros(mn,1);
+T=at+(0:N)*k;
 
-for i=2 :m-1 % interior points
+A=zeros(mn,mn);
+b=zeros(mn,1);
+
+for i=2 :m-1 
 
 for j=2:n-1
 
-A(i+(j-1)*m,i-1+(j-1)*m)=1/h2;
-A(i+(j-1)*m,i+1+(j-1)*m)=1/h2;
+A(i+(j-1)*m,i-1+(j-1)*m)=1/h^2;
+A(i+(j-1)*m,i+1+(j-1)*m)=1/h^2;
 
-A(i+(j-1) *m,i+(j-1)*m)=-2/h2-2/k2;
+A(i+(j-1) *m,i+(j-1)*m)=-2/h^2-2/k^2;
 
-A(i+(j-1)*m,i+(j-2)*m)=1/k2;
-A(i+(j-1)*m,i+j*m)=1/k2;
+A(i+(j-1)*m,i+(j-2)*m)=1/k^2;
+A(i+(j-1)*m,i+j*m)=1/k^2;
 
-b(i+(j-1)*m)=f(x(i),y(j));
+b(i+(j-1)*m)=f(X(i),T(j));
 
 end
 
@@ -47,17 +49,20 @@ end
 
 for i=1:m % bottom and top boundary points
 
-j=1; A(i+(j-1)*m,i+(j-1)*m)=1;b(i+(j-1)*m)=g1(x(i));
+A(i,i)=1;
+b(i)=g1(X(i));
 
-j=n;A(i+(j-1)*m,i+(j-1)*m)=1;b(i+(j-1)*m)=g2(x(i));
+A(i+(n-1)*m,i+(n-1)*m)=1;
+b(i+(n-1)*m)=g2;
 
 end
 
 for j=2:n-1 % left and right boundary points
 
-i=1;A(i+(j-1)*m,i+(j-1)*m)=1;b(i+(j-1)*m)=g3(y(j));
-
-i=m;A(i+(j-1)*m,i+(j-1)*m)=1;b(i+(j-1)*m)=g4(y(j));
+A(1+(j-1)*m,1+(j-1)*m)=1;
+b(1+(j-1)*m)=g3;
+A(m+(j-1)*m,m+(j-1)*m)=1;
+b(m+(j-1)*m)=g4;
 
 end
 
@@ -65,5 +70,5 @@ v=A\b;% solve for solution in v labeling
 
 w=reshape(v(1:mn),m,n); %translate from v to w
 
-mesh(x,y,w)
+mesh(X,T,w)
 
